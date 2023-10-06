506184bdc83730272e8dc438cbbe556b699f4971
GabrielSevk8 <135348439+GabrielSevk8@users.noreply.github.com>
2023-10-06 11:27:15 -0300

    VScode.atividade 13 - 06.10.2023

---
+++ dom.js
@@ -0,0 +1,13 @@
+
+function clickMe(){
+    document.querySelector('.box').innerHTML = '<h1>Clicou!</h1>'
+}
+
+function entering(){
+    document.querySelector('.box').innerHTML = '<h1>Entrou!</h1>'
+}
+
+function outing(){
+    document.querySelector('.box').innerHTML = '<h1>Saiu!</h1'
+}
+
